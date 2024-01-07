f82bd478ce9265659c36ce98811c0fbde79d9dc0
Rama Vidya Sulaiman <rvidyasulaiman@gmail.com>
2024-01-07 20:45:44 +0700

    [INITIAL COMMIT] initial commit

---
+++ src/main/java/com/enigma/ClassNexa/ClassNexaApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.ClassNexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassNexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassNexaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/ClassNexa/ClassNexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.ClassNexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassNexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
